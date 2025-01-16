6ab2fdc7af1841afd18af882f80c89b655a30cc1
andrearj19 <aaarias19@utpl.edu.ec>
2025-01-16 12:50:23 -0500

    cambios

---
+++ EjemploTres/src/paquete03/Ejemplo03.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejemplo03 {
+        public static void main(String[] args) {
+            String[] dataInicial = {"100", "90", "1n", "10", "H1", "H2"};
+           
+            int [] datoFinal = new int[6];
+            for(int i = 0; i<datoFinal.length;i++){
+                datoFinal [i]= NumberUtils.toInt(dataInicial[i]);
+        }
+              for(int i = 0; i<datoFinal.length;i++){
+                  System.out.printf("%d\n", datoFinal[i]);
+              }
+        
+            
+            
+            
+           
+
+        }
+  
+}
